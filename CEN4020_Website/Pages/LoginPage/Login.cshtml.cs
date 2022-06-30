aaec09e6c87c82e4620a77d4b166565782ccec06
jburgos115 <burgosjonah@gmail.com>
2022-06-30 11:25:57 -0400

    Added login page

---
+++ CEN4020_Website/Pages/LoginPage/Login.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace CEN4020_Website.Pages.Login
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public LoginInfo? Login { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+
+    public class LoginInfo
+    {
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
