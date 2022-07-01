70bfc107346c17731a973aed0e3157351c804e25
Jonah Burgos <jonahb1@usf.edu>
2022-07-01 17:39:07 -0400

    Moved Author page to Author folder and created author input page

---
+++ CEN4020_Website/Pages/Author/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CEN4020_Website.Pages.Author
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
