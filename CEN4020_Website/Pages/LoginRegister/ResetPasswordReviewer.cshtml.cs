ac5620f334e954955ddf3375cee93a2719f12b8a
David Alvarez <davidj398@yahoo.com>
2022-07-12 14:41:52 -0400

    Added Reset Password Email

---
+++ CEN4020_Website/Pages/LoginRegister/ResetPasswordReviewer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CEN4020_Website.Pages.LoginRegister
+{
+    public class ResetPasswordReviewerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CEN4020_Website/Pages/Services/ResetMessageSenderOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace CEN4020_Website.Pages.Services
+{
+    public class ResetMessageSenderOptions
+    {
+        public string SendGridKey { get; set; }
+        public string SenderEmail { get; set; }
+        public string SenderName { get; set; }
+    }
+}
