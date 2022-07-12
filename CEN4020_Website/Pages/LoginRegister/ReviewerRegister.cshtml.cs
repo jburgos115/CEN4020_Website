5f44444f40e6071cedee0501d5f52e8fe07bea9f
jburgos115 <burgosjonah@gmail.com>
2022-07-12 00:38:13 -0400

    reviewer register

---
+++ CEN4020_Website/Pages/LoginRegister/ReviewerRegister.cshtml.cs
@@ -0,0 +1,37 @@
+using CEN4020_Website.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CEN4020_Website.Pages.Reviewers
+{
+    [BindProperties]
+    public class RegisterModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        
+        public Model.Reviewer Reviewer { get; set; }
+
+        public RegisterModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Reviewer.AddAsync(Reviewer);
+                await _db.SaveChangesAsync();
+                TempData["success"] = "Registration was Successful";
+                return RedirectToPage("/LoginRegister/Login");
+            }
+
+            return Page();
+        }
+    }
+}
